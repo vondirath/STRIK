19e696f32b7a17484204e764f8839905837c6c65
Thomas Lopez <Vondirath@gmail.com>
2017-01-28 16:14:04 -0700

    feature started database setup

--- STRIK/app/models.py
+++ STRIK/app/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
-
 from django.db import models
+import datetime
 
-# Create your models here.
+
+
+class Posts(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    post_date = models.DateField('date published')
+    image_name = models.CharField(max_length=50)
+    sale = models.BooleanField(default=False)
+    featured = models.BooleanField(default=False)
+    price = models.CharField(max_length=4)
+    temporary_price = models.CharField(max_length=4)
+
