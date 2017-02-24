e8c7720c04a870e0be6bab08b21cdcffd92352b6
Thomas Lopez <Vondirath@gmail.com>
2017-02-24 10:13:46 -0700

    docs updated form sheet

--- STRIK/app/forms.py
+++ STRIK/app/forms.py
@@ -1,16 +1,6 @@
 from django import forms
 
 
-class PostForm(forms.Form):
-    title = forms.CharField(label="title", max_length=100)
-    description = forms.CharField(label="description", max_length=200)
-    image_name = forms.CharField(label="image_name", max_length=50)
-    sale = forms.BooleanField(required=False)
-    featured = forms.BooleanField(required=False)
-    price = forms.CharField(label="price", max_length=4)
-    temporary_price = forms.CharField(label="temporary_price", max_length=4, required=False)
-
-
 class ContactForm(forms.Form):
     """
         Returns the content to be rendered for the form page.
